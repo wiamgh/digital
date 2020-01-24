6b6aaca5f11870d24cef4bca28eaf84b55b55e95
Wiam <wiaam.gh@gmail.com>
2020-01-24 14:12:09 +0100

    Initial commit

---
+++ digital/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DigitalItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    authors = scrapy.Field()
+    country = scrapy.Field()
+    abstract_ = scrapy.Field()
+    date_pub = scrapy.Field()
+    journal = scrapy.Field()
+    topic = scrapy.Field()
+    latitude = scrapy.Field()
+    longtitude = scrapy.Field()
